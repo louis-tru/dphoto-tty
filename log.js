96f8933da93a0361244bee2f6554920e32b130d5
louis <louistru@hotmail.com>
2018-12-10 21:44:36 +0800

    update log

--- log.js
+++ log.js
@@ -9,9 +9,15 @@ var log = require('ngui-utils/log');
 
 if (!log.defaultConsole) {
 	if (!utils.dev || utils.options.caught_exception) {
-		process.on('uncaughtException', function(e) {
-			console.error(`Caught exception: ${e}\n`);
+
+		process.on('uncaughtException', function(err) {
+			console.error(err);
 		});
+
+		process.on('unhandledRejection', (err, promise) => {
+			console.error(err);
+		});
+
 	}
 	new log.Console(paths.var + '/log.txt').makeDefault();
 }
